23952198c57910e611bb4d0a08b34432396e6e6b
Alfredmakura <makuraalfred92@gmail.com>
2025-05-21 09:28:04 +0300

    Update k.js

--- k.js
+++ k.js
@@ -1,1 +1,21 @@
 let age = 20
+//primitive datatypes 
+//1. string 
+let name ="Alfred"
+let name2 ='trip'
+//2.number 
+let age2 =20;//number
+let n=2.5//-2.5
+//3: Boolean(true or false)
+let married= true;
+let hascar= false;
+//4.null (absense of value )
+let x =null;
+let y =  ifined;
+console.log("age")
+function check weather (){
+    let issunny = true;
+    if (issunny) {
+        return "wear sunnglasses";
+    }
+}
